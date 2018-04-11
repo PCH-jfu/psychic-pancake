ec66627ce8815cbfc37edfb841845ff5f9cacc6d
PCH-jfu <justin.fu@pchintl.com>
2018-04-11 13:12:06 +0800

    add variable new_branch

--- main.c
+++ main.c
@@ -3,13 +3,18 @@
 int a;
 int b;
 int c;
+int new_branch;
 
 void main(){
 	int d;
 	a = 1;
-	b = 2;
+	b = 200;
 	c = 3;
 	d = a + b + c;
+
+	new_branch = a + d;
+	
 	printf("the value of d is %d", d);
 
+	printf("the new_branch value is %d", new_branch);
 }
