7558d1c85393ba3e2d22126dab178ed274703ae9
PCH-jfu <justin.fu@pchintl.com>
2018-04-11 13:18:58 +0800

    Add f

--- main.c
+++ main.c
@@ -7,13 +7,16 @@ int new_branch;
 
 void main(){
 	int d;
+	int f;
 	a = 1;
 	b = 200;
 	d = a + b + b;
 
 	new_branch = a + d;
 	
-	printf("the value of d is %d", d);
+	printf("the value of d is %d\r\n", d);
 
-	printf("the new_branch value is %d", new_branch);
+	printf("the new_branch value is %d\r\n", new_branch);
+
+	printf("we don't use f\r\n");
 }
